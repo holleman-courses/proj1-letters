0431b9a6adbab0d2728806b26beb7e64a80d57b2
nathon-tadeo <nathontadeo@gmail.com>
2025-03-27 00:50:36 -0400

    header file

---
+++ embedded/lib/mug_model_trained/mug_model.h
@@ -0,0 +1,9 @@
+#ifndef MUG_MODEL_H
+#define MUG_MODEL_H
+
+#include <Arduino.h>
+
+extern const unsigned char mug_detect_model_data[];
+extern const int mug_detect_model_data_len;
+
+#endif  
--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -5,7 +5,7 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
-
+Dresden, Germanydresden
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,11 +16,12 @@ limitations under the License.
 #include <TensorFlowLite.h>
 
 #include "main_functions.h"
-
 #include "detection_responder.h"
 #include "image_provider.h"
 #include "model_settings.h"
 #include "person_detect_model_data.h"
+
+
 #include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
